40408bb5a1bd4787b5235fdf4a24edd17e62b141
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-12-01 17:23:23 -0500

    join leave logging

--- robot.py
+++ robot.py
@@ -26,7 +26,26 @@ logging.getLogger().addHandler(logging.StreamHandler())
 @bot.event
 async def on_ready():
     log_info(f"Logged in as: {bot.user.name} : {str(bot.user.id)}")
-    
+    game = discord.Game(name=f"on {str(len(bot.guilds))} guilds!")
+    await bot.change_presence(activity=game)
+
+@bot.event
+async def on_guild_join(guild):
+    log_info(f"Joined server `{guild.name}`!")
+    log_info(f"Connected to: {str(len(bot.guilds))} guilds.")
+    log_info(f"Connected to: {str(len(bot.users))} users.")
+    game = discord.Game(name=f"on {str(len(bot.guilds))} guilds!")
+    await bot.change_presence(activity=game)
+
+
+@bot.event
+async def on_guild_remove(guild):
+    log_info(f"Left server `{guild.name}`.")
+    log_info(f"Connected to: {str(len(bot.guilds))} guilds.")
+    log_info(f"Connected to: {str(len(bot.users))} users.")
+    game = discord.Game(name=f"on {str(len(bot.guilds))} guilds!")
+    await bot.change_presence(activity=game)
+
 async def main():
     for cog in cogs:
         try:
